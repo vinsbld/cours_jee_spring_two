42acabb655c04f6882bfd4fb5c8a2a15266452e4
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 14:54:17 +0200

    3_mappingEntitiesClassRetrait

--- maBanque/src/main/java/org/sid/entities/Retrait.java
+++ maBanque/src/main/java/org/sid/entities/Retrait.java
@@ -2,6 +2,10 @@ package org.sid.entities;
 
 import java.util.Date;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("R")
 public class Retrait extends Operation {
 
 	public Retrait() {
