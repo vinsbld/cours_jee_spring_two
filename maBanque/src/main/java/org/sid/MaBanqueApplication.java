b372908af414be28fc5a4f99a77b233b85e7964c
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 17:09:47 +0200

    8_configMaBanqueApp_for_add_newOperation_with_secondeMethode

--- maBanque/src/main/java/org/sid/MaBanqueApplication.java
+++ maBanque/src/main/java/org/sid/MaBanqueApplication.java
@@ -6,10 +6,14 @@ import java.util.Date;
 
 import org.sid.dao.ClientRepository;
 import org.sid.dao.CompteRepository;
+import org.sid.dao.OperationRepository;
 import org.sid.entities.Client;
 import org.sid.entities.Compte;
 import org.sid.entities.CompteCourant;
 import org.sid.entities.CompteEpargne;
+import org.sid.entities.Operation;
+import org.sid.entities.Retrait;
+import org.sid.entities.Versement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,6 +26,8 @@ public class MaBanqueApplication implements CommandLineRunner {
 	private ClientRepository clientRepository;
 	@Autowired
 	private CompteRepository compteRepository;
+	@Autowired
+	private OperationRepository operationRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaBanqueApplication.class, args);
@@ -35,6 +41,14 @@ public class MaBanqueApplication implements CommandLineRunner {
 		
 		Compte cp1 = compteRepository.save(new CompteCourant("c1", new Date(), 90000, c1, 6000));
 		Compte cp2 = compteRepository.save(new CompteEpargne("c2", new Date(), 6000, c2, 5.5));
+		
+		operationRepository.save(new Versement(new Date(), 9000, cp1));
+		operationRepository.save(new Retrait(new Date(), 6000, cp1));
+		operationRepository.save(new Versement(new Date(), 2300, cp1));
+		
+		operationRepository.save(new Versement(new Date(), 9000, cp2));
+		operationRepository.save(new Retrait(new Date(), 6000, cp2));
+		operationRepository.save(new Versement(new Date(), 2300, cp2));
 	}
 
 }
