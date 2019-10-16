666f0095f069df634356346a209318f754f5f752
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 18:08:44 +0200

    9_created_metierIterface

---
+++ maBanque/src/main/java/org/sid/metier/IBanqueMetier.java
@@ -0,0 +1,14 @@
+package org.sid.metier;
+
+import org.sid.entities.Compte;
+import org.sid.entities.Operation;
+import org.springframework.data.domain.Page;
+
+public interface IBanqueMetier {
+	public Compte consulterCompte (String codeCpte); 
+	public void verser(String codeCpte, double montant);
+	public void retirer(String codeCpte, double montant);	
+	public void virement(String codeCpte1,String codeCpte2, double montant);
+	//conulter les opérations d'un compte, page par page
+	public Page<Operation> listOperation(String codeCpte, int page, int size);
+}
