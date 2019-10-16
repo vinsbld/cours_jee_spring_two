a88e56c525ebaeeb3210276df05e15f7db0864e9
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 16:56:36 +0200

    7_configMaBanqueApp_for_add_newCompteClient_with_secondeMethode

--- maBanque/src/main/java/org/sid/MaBanqueApplication.java
+++ maBanque/src/main/java/org/sid/MaBanqueApplication.java
@@ -1,7 +1,15 @@
 package org.sid;
 
+
+
+import java.util.Date;
+
 import org.sid.dao.ClientRepository;
+import org.sid.dao.CompteRepository;
 import org.sid.entities.Client;
+import org.sid.entities.Compte;
+import org.sid.entities.CompteCourant;
+import org.sid.entities.CompteEpargne;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +20,8 @@ import org.springframework.context.ApplicationContext;
 public class MaBanqueApplication implements CommandLineRunner {
 	@Autowired
 	private ClientRepository clientRepository;
+	@Autowired
+	private CompteRepository compteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaBanqueApplication.class, args);
@@ -22,6 +32,9 @@ public class MaBanqueApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Client c1 = clientRepository.save(new Client("Hassan", "hassan@gmail.com"));
 		Client c2 = clientRepository.save(new Client("Vincent", "vincent@gmail.com"));
+		
+		Compte cp1 = compteRepository.save(new CompteCourant("c1", new Date(), 90000, c1, 6000));
+		Compte cp2 = compteRepository.save(new CompteEpargne("c2", new Date(), 6000, c2, 5.5));
 	}
 
 }
