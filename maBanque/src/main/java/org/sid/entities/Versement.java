1a01acd7c73dbf851af734d20bd4aa034652f7df
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 14:53:07 +0200

    3_mappingEntitiesClassVersement

--- maBanque/src/main/java/org/sid/entities/Versement.java
+++ maBanque/src/main/java/org/sid/entities/Versement.java
@@ -2,6 +2,10 @@ package org.sid.entities;
 
 import java.util.Date;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("V")
 public class Versement extends Operation {
 
 	public Versement() {
