d048d291a975de81062b8b568fd85df1e0ed2e3b
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 16:39:12 +0200

    6_configMaBanqueApp_for_add_newClient_firstMethode

--- maBanque/src/main/java/org/sid/MaBanqueApplication.java
+++ maBanque/src/main/java/org/sid/MaBanqueApplication.java
@@ -1,13 +1,20 @@
 package org.sid;
 
+import org.sid.dao.ClientRepository;
+import org.sid.entities.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MaBanqueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MaBanqueApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(MaBanqueApplication.class, args);
+		//avec l'Objet clientResository on pourra gerer des clients
+		ClientRepository clientRepository = ctx.getBean(ClientRepository.class);
+		//ajuout de clients avec la methode save
+		clientRepository.save(new Client("Hassan", "hassan@gmail.com"));
 	}
 
 }
