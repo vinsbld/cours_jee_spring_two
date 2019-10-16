f75e395c6310f34ba6e9d8299099979fa7478418
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-16 16:47:07 +0200

    6_configMaBanqueApp_for_add_newClient_secondeMethode

--- maBanque/src/main/java/org/sid/MaBanqueApplication.java
+++ maBanque/src/main/java/org/sid/MaBanqueApplication.java
@@ -2,19 +2,26 @@ package org.sid;
 
 import org.sid.dao.ClientRepository;
 import org.sid.entities.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class MaBanqueApplication {
+public class MaBanqueApplication implements CommandLineRunner {
+	@Autowired
+	private ClientRepository clientRepository;
 
 	public static void main(String[] args) {
-		ApplicationContext ctx =  SpringApplication.run(MaBanqueApplication.class, args);
-		//avec l'Objet clientResository on pourra gerer des clients
-		ClientRepository clientRepository = ctx.getBean(ClientRepository.class);
-		//ajuout de clients avec la methode save
-		clientRepository.save(new Client("Hassan", "hassan@gmail.com"));
+		SpringApplication.run(MaBanqueApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Client c1 = clientRepository.save(new Client("Hassan", "hassan@gmail.com"));
+		Client c2 = clientRepository.save(new Client("Vincent", "vincent@gmail.com"));
 	}
 
 }
