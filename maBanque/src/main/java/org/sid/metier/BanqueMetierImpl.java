b74d173329865343e9d32ff54df8df239cf626a1
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-17 09:57:36 +0200

    10_implementsMetierInterface_doVersement

--- maBanque/src/main/java/org/sid/metier/BanqueMetierImpl.java
+++ maBanque/src/main/java/org/sid/metier/BanqueMetierImpl.java
@@ -1,10 +1,13 @@
 package org.sid.metier;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.sid.dao.CompteRepository;
+import org.sid.dao.OperationRepository;
 import org.sid.entities.Compte;
 import org.sid.entities.Operation;
+import org.sid.entities.Versement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -15,38 +18,45 @@ import org.springframework.transaction.annotation.Transactional;
 //@Transactional ttes les methodes sont transactionnelles
 @Service
 @Transactional
-public class BanqueMetierImpl implements IBanqueMetier  {
-	//appel de la couche dao
+public class BanqueMetierImpl implements IBanqueMetier {
+	// appel de la couche dao
 	@Autowired
 	private CompteRepository compteRepository;
+	@Autowired
+	private OperationRepository operationRepository;
 
 	@Override
 	public Compte consulterCompte(String codeCpte) {
-		//pour consulter un compte
-		Optional<Compte> compte=compteRepository.findById(codeCpte);
+		// pour consulter un compte
+		Optional<Compte> compte = compteRepository.findById(codeCpte);
 		Compte cp = null;
-		 if(compte.isPresent()) {
-	            cp = compte.get();
-	        }else throw new RuntimeException("Compte introuvable");
-	        return cp;
+		if (compte.isPresent()) {
+			cp = compte.get();
+		} else
+			throw new RuntimeException("Compte introuvable");
+		return cp;
 	}
 
 	@Override
 	public void verser(String codeCpte, double montant) {
-		// TODO Auto-generated method stub
-		
+		// effectuer un versement
+		Compte cp = consulterCompte(codeCpte);
+		Versement v = new Versement(new Date(), montant, cp);
+		operationRepository.save(v);
+		cp.setSolde(cp.getSolde()+montant);
+		compteRepository.save(cp);
 	}
 
 	@Override
 	public void retirer(String codeCpte, double montant) {
-		// TODO Auto-generated method stub
-		
+		// effectuer un retrait
+	
 	}
 
 	@Override
 	public void virement(String codeCpte1, String codeCpte2, double montant) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
