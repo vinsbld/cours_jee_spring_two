c494ac82df446a833fd16a71718e4cb883a47947
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-18 19:39:28 +0200

    17_addInfoCompteOnFront&Back

--- maBanque/src/main/java/org/sid/web/BanqueController.java
+++ maBanque/src/main/java/org/sid/web/BanqueController.java
@@ -1,8 +1,12 @@
 package org.sid.web;
 
+import org.sid.entities.Compte;
+import org.sid.entities.Operation;
 import org.sid.metier.IBanqueMetier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +17,22 @@ public class BanqueController {
 
 	@RequestMapping("/operations")
 	public String index() {
+		return "comptes";
+	}
+	
+	@RequestMapping("/consulterCompte")
+	public String consulter(Model model, String codeCompte) {
+		model.addAttribute("codeCompte", codeCompte);
+
+		try {
+			Compte cp = banqueMetier.consulterCompte(codeCompte);
+			Page<Operation> pageOperation=banqueMetier.listOperation(codeCompte, 0, 4);
+			model.addAttribute("listOperations", pageOperation.getContent());
+			model.addAttribute("compte",cp);
+		} catch (Exception e) {
+			model.addAttribute("exception",e);
+		}
+
 		return "comptes";
 	}
 }
