2f1d50ab4a695e1c1e7c307e553c83b57438a6bc
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-23 14:26:07 +0200

    02_loginForm_rigthAcess

--- maBanque/src/main/java/org/sid/security/SecurityConfig.java
+++ maBanque/src/main/java/org/sid/security/SecurityConfig.java
@@ -1,5 +1,22 @@
 package org.sid.security;
 
-public class SecurityConfig {
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN","USER");
+		auth.inMemoryAuthentication().withUser("user").password("1234").roles("USER");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.authorizeRequests().antMatchers("/operations","/consulterCompte").hasRole("USER");
+		http.authorizeRequests().antMatchers("/saveOperation").hasRole("ADMIN");
+	}
 }
