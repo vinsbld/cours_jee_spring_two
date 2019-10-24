e6021eabcbc9306788582f26f567ed451530722c
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-24 09:55:20 +0200

    06_affichageBugFixed

--- maBanque/src/main/java/org/sid/security/SecurityConfig.java
+++ maBanque/src/main/java/org/sid/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.formLogin().loginPage("/login");
+        http.formLogin();
         http.authorizeRequests().antMatchers("/operations", "/consulterCompte").hasRole("USER");
         http.authorizeRequests().antMatchers("/saveOperation").hasRole("ADMIN");
     }
